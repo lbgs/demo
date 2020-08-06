e5e572d015a386183e8510ed511718ac0de5e8bc
王荣华 <18823547785@163.com>
2020-08-06 10:48:47 +0800

    add tj.js.

---
+++ tj.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?96797be5057f773cafa71466303240eb";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
+</script>
